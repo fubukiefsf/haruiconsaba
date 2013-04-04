b8023dd1c53fe8ea73faced9c20daeb61ce431cc
fubukiefsf <fubuki.efsf11@gmail.com>
2013-04-05 03:09:44 +0900

    0.1

---
+++ haruiconsaba.rb
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+require 'json'
+require 'open-uri' 
+Plugin.create(:haruiconsaba) do
+	haruiconApiUri = "http://api.haruicon.com/?g="
+	tOfh = "さん"
+	tag = " #はるアイコン鯖 "
+	command(
+		:haruiconsaba,
+		name: "はるアイコン鯖のログイン者をつぶやく",
+		visible: false,
+		icon: "#{File.dirname(__FILE__)}/haruicon.png",
+		condition: lambda{|opt| true},
+		role: :window
+	 ) do |opt|
+		Thread.new{
+			twiIdInList = JSON.parse(open(haruiconApiUri + "twiIdInList").read)
+			if twiIdInList['playerCount'] != 0
+				mes = ""
+				twiIdInList['idList'].each{|ids|
+					mes += ids['mcId'] + tOfh
+				}
+				mes += "がインしています" + tag
+				Post.primary_service.update(:message => mes)
+			end
+		}
+	end
+end
---
+++ spec
@@ -0,0 +1,9 @@
+---
+slug: :haruiconsaba
+depends:
+  mikutter: 0.2.1.1141
+  plugin: []
+version: '0.1'
+author: はるアイコン鯖ログイン者確認
+name: fubukiefsf
+description: mikutterではるアイコン鯖に今いる人をつぶやきます
